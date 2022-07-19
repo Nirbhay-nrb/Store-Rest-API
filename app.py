25c73cadff64a6ac2c2626a67f562bbe17d6f30b
Nirbhay <nmakhija_be20@thapar.edu>
2022-07-19 23:01:23 +0530

    test run

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+# import os
 
 from datetime import timedelta
 from flask import Flask
@@ -14,7 +14,8 @@ from db import db
 # creating app and api
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL' , 'sqlite:///data.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL' , 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 # if the get method return None (that is the app is not running on heroku's computer), then the data.db on our computer will be used
 # telling the app from where to find the data.db file 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # turns off the Flask_sqlalchemy tracker for changes as it cost some resources
